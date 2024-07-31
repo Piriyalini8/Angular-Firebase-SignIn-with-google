ec2bb23b7da27d57e42a040732b5588d6b47d02a
Piriyalini Niththiyanantham <priyalini.18@itfac.mrt.ac.lk>
2024-07-31 14:49:30 +0530

    SignInwith popup only work front end. gives wrong token

--- src/app/firebase-config.ts
+++ src/app/firebase-config.ts
@@ -1,11 +1,25 @@
 // src/app/firebase-config.ts
 import { initializeApp } from 'firebase/app';
-import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from 'firebase/auth';
+import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
 import { environment } from '../environments/environment';
 
-// Initialize Firebase
 const app = initializeApp(environment.firebaseConfig);
 const auth = getAuth(app);
 const provider = new GoogleAuthProvider();
 
-export { auth, provider, signInWithPopup, signOut };
+export const getGoogleToken = async () => {
+  try {
+    const result = await signInWithPopup(auth, provider);
+    console.log('Result: '+result);
+    const credential = GoogleAuthProvider.credentialFromResult(result);
+    console.log('Credential; '+credential);
+    if (credential) {
+      return credential.accessToken;
+    } else {
+      throw new Error('Failed to retrieve Google token');
+    }
+  } catch (error) {
+    console.error('Token retrieval error:', error);
+    throw error;
+  }
+};
